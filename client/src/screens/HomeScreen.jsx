7abf9fd9a6a578bbcfe87365bd0dd81ca20c8858
HacAtac <jhack00@icloud.com>
2022-03-26 23:07:21 -0500

    served data to the client and .map through for home page and did product by id page

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Row, Col, AccordionCollapse } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      //data is the response from the server. It's an array of products.
+
+      setProducts(data) //this is setting the state of the products to the data from the server. so that the products are now available in the state.
+      //later we will use redux to store the products in the store or `global state`
+    }
+
+    fetchProducts()
+  }, [])
+
   return (
     <>
       <h1>Latest Products</h1>
