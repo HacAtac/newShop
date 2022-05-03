b71df7abf3a657bff2bab58ae66c21cf7435c019
Jordan Hackworth <jhack00@icloud.com>
2022-05-02 23:06:50 -0500

    Fixes ........................ BLEH

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
-import path from 'path';
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-// import colors from colors
-
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
@@ -13,15 +12,11 @@ import uploadRoutes from './routes/uploadRoutes.js'
 dotenv.config()
 
 connectDB()
-
 const app = express()
-
 app.use(express.json())
-
 app.get('/', (req, res) => {
-  res.send('API is running...')
+  res.send('API is running....')
 })
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -31,8 +26,8 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
-const __dirname = path.resolve() //this is here because were using Es modules instead of CommonJS
-app.use('/uploads', express.static(path.join(__dirname, '/uploads'))) //only avaible with commonJS which is the require syntax
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
@@ -41,7 +36,6 @@ const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-      .bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 )
