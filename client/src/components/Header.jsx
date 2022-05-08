a57787b57f3c04866f0b08f9c41596ff61a5d048
HacAtac <jhack00@icloud.com>
2022-05-08 16:38:35 -0500

    Product Search Functionality Server+Client

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import SearchBox from './SearchBox'
 import { logout } from '../actions/userActions'
 
 const Header = () => {
   const dispatch = useDispatch()
-
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
-
   const logoutHandler = () => {
     dispatch(logout())
   }
-
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -23,22 +22,20 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
+            <Route render={({ history }) => <SearchBox history={history} />} />
             <Nav className='ml-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <i className='fas fa-shopping-cart'></i> Cart
                 </Nav.Link>
               </LinkContainer>
-
               {userInfo ? (
                 <NavDropdown title={userInfo.name} id='username'>
                   <LinkContainer to='/profile'>
-                    <NavDropdown.Item>
-                      <i className='fas fa-user'></i> Profile
-                    </NavDropdown.Item>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
                   <NavDropdown.Item onClick={logoutHandler}>
-                    <i className='fas fa-sign-out-alt'></i> Logout
+                    Logout
                   </NavDropdown.Item>
                 </NavDropdown>
               ) : (
@@ -51,21 +48,13 @@ const Header = () => {
               {userInfo && userInfo.isAdmin && (
                 <NavDropdown title='Admin' id='adminmenu'>
                   <LinkContainer to='/admin/userlist'>
-                    <NavDropdown.Item>
-                      <i className='fas fa-user'></i> Users
-                    </NavDropdown.Item>
+                    <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
-
                   <LinkContainer to='/admin/productlist'>
-                    <NavDropdown.Item>
-                      <i className='fas fa-box'></i> Products
-                    </NavDropdown.Item>
+                    <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
-
                   <LinkContainer to='/admin/orderlist'>
-                    <NavDropdown.Item>
-                      <i className='fas fa-box'></i> Orders
-                    </NavDropdown.Item>
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
                   </LinkContainer>
                 </NavDropdown>
               )}
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -5,7 +5,16 @@ import Product from '../models/productModel.js'
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({})
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {}
+
+  const products = await Product.find({ ...keyword })
 
   res.json(products)
 })
@@ -105,7 +114,7 @@ const createProductReview = asyncHandler(async (req, res) => {
     }
 
     const review = {
-      name: req.user.name, //"message": "Cannot read properties of null (reading 'name')", because req.user is null 
+      name: req.user.name, //"message": "Cannot read properties of null (reading 'name')", because req.user is null
       rating: Number(rating),
       comment,
       user: req.user._id,
