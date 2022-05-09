dc786b2aa1aa48c0a261b1a2903ca569b942f686
HacAtac <jhack00@icloud.com>
2022-05-08 19:38:48 -0500

    hmm v2

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose'
-import colors from 'colors'
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
       useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
     })
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
